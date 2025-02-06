589debf4ad804c95d7412bf8621cc09b1c4acc38
Nivedita Pal <npal4661@gmail.com>
2025-02-06 23:06:38 +0530

    updated the repository with task 13 in which sum of all random values present in file is printed

--- src/com/Nivedita/Task13/RandomNumberFile.java
+++ src/com/Nivedita/Task13/RandomNumberFile.java
@@ -7,24 +7,29 @@ public class RandomNumberFile {
     public static void main(String[] args) throws IOException {
         String file = "numbers.txt";
         //writing random values to a text file
-        try {
-            BufferedWriter br = new BufferedWriter(new FileWriter(file));
-            Random random = new Random();
-            for(int i=0; i<100; i++){
-                int num = random.nextInt(100);
-                br.write(num + "\n");
-                br.flush();
-            }
-        }catch (IOException e){
-            System.out.println(e.getMessage());
-        }
-        //summing the values written in the file
-//        try{
-//            BufferedReader br = new BufferedReader(new FileReader(file));
-//
+//        try {
+//            BufferedWriter br = new BufferedWriter(new FileWriter(file));
+//            Random random = new Random();
+//            for(int i=0; i<100; i++){
+//                int num = random.nextInt(100);
+//                br.write(num + "\n");
+//                br.flush();
+//            }
 //        }catch (IOException e){
 //            System.out.println(e.getMessage());
 //        }
+        //summing the values written in the file
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            int sum = 0;
+            String read;
+            while((read = br.readLine())!=null){
+                sum += Integer.parseInt(read);
+            }
+            System.out.println(sum);
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
 
     }
 }
